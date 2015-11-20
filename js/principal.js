38646e15c16903f7d42dc5ad778c8fb603edece2
Ivanelson Nunes <ivanelsonnunes@gmail.com>
2015-11-19 22:58:19 -0300

    Altera textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
-var banners = ["Os melhores do Brasil!", "Qualidade e preco baixao!"];
+var banners = ["Do lixo ao luxo!", "Reaproveitar eh aproveitar!"];
 var bannerAtual = 0;
 
 function trocaBanner(){
